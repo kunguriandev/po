d76221ca01003cb368ef4e577a4969168a2acb5d
Krzysztof <287130@student.pwr.edu.pl>
2025-03-10 16:27:26 +0100

    basic project setup, cmake lists, hello world

---
+++ build/CMakeFiles/obiektowe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/obiektowe.dir/src/main.cpp.o"
+  "CMakeFiles/obiektowe.dir/src/main.cpp.o.d"
+  "obiektowe"
+  "obiektowe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/obiektowe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+  std::cout<<"kdwoakdo"<<std::endl;
+  return 0;
+}
